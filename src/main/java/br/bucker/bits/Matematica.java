181cd29ba01c62c1a0ac652dbe9a0cc6f1e9142c
brunobuckeridge <bruno.buckeridge@gmail.com>
2024-11-21 12:55:00 -0300

    Separar a matematica do Controller

---
+++ src/main/java/br/bucker/bits/Matematica.java
@@ -0,0 +1,31 @@
+package br.bucker.bits;
+
+public class Matematica {
+
+	public Matematica() {
+	}
+
+	public Double soma(Double numberOne, Double numberTwo) throws Exception {
+		return numberOne + numberTwo;
+	}
+	
+	public Double sub(Double numberOne, Double numberTwo) throws Exception {
+		return numberOne - numberTwo;
+	}
+	
+	public Double mult(Double numberOne, Double numberTwo) throws Exception {
+		return numberOne * numberTwo;
+	}
+	
+	public Double div(Double numberOne, Double numberTwo) throws Exception {
+		return numberOne / numberTwo;
+	}
+	
+	public Double med(Double numberOne, Double numberTwo) throws Exception {
+		return (numberOne + numberTwo) / 2;
+	}
+	
+	public Double raiz(Double number) throws Exception {
+		return Math.sqrt(number);
+	}
+}
