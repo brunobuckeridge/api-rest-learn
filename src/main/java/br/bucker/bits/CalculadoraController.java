9c78f1f1c966d1481582479f79335f465856bcd4
brunobuckeridge <bruno.buckeridge@gmail.com>
2024-11-21 12:37:47 -0300

    Separação de logica e controller

--- src/main/java/br/bucker/bits/CalculadoraController.java
+++ src/main/java/br/bucker/bits/CalculadoraController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.bucker.bits.exceptions.UnsupportedMathOperationException;
+import br.bucker.bits.utils.DoubleUtils;
 
 @RestController
 public class CalculadoraController {
@@ -13,51 +13,33 @@ public class CalculadoraController {
 
 	@GetMapping("/soma/{numberOne}/{numberTwo}")
 	public String soma(@PathVariable String numberOne, @PathVariable String numberTwo) throws Exception {
-		return  String.format(template, "soma", validaNumero(numberOne) + validaNumero(numberTwo));
+		return  String.format(template, "soma", DoubleUtils.validaNumero(numberOne) + DoubleUtils.validaNumero(numberTwo));
 	}
 	
 	@GetMapping("/sub/{numberOne}/{numberTwo}")
 	public String sub(@PathVariable String numberOne, @PathVariable String numberTwo) throws Exception {
-		return  String.format(template, "subtração", validaNumero(numberOne) - validaNumero(numberTwo));
+		return  String.format(template, "subtração", DoubleUtils.validaNumero(numberOne) - DoubleUtils.validaNumero(numberTwo));
 	}
 	
 	@GetMapping("/mult/{numberOne}/{numberTwo}")
 	public String mult(@PathVariable String numberOne, @PathVariable String numberTwo) throws Exception {
-		return  String.format(template, "multiplicação", validaNumero(numberOne) * validaNumero(numberTwo));
+		return  String.format(template, "multiplicação", DoubleUtils.validaNumero(numberOne) * DoubleUtils.validaNumero(numberTwo));
 	}
 	
 	@GetMapping("/div/{numberOne}/{numberTwo}")
 	public String div(@PathVariable String numberOne, @PathVariable String numberTwo) throws Exception {
-		return  String.format(template, "divisão", validaNumero(numberOne) / validaNumero(numberTwo));
+		return  String.format(template, "divisão", DoubleUtils.validaNumero(numberOne) / DoubleUtils.validaNumero(numberTwo));
 	}
 	
 	@GetMapping("/med/{numberOne}/{numberTwo}")
 	public String med(@PathVariable String numberOne, @PathVariable String numberTwo) throws Exception {
-		return  String.format(template, "media", (validaNumero(numberOne) + validaNumero(numberTwo)) / 2);
+		return  String.format(template, "media", (DoubleUtils.validaNumero(numberOne) + DoubleUtils.validaNumero(numberTwo)) / 2);
 	}
 	
-	@GetMapping("/raiz/{numberOne}")
-	public String raiz(@PathVariable String numberOne) throws Exception {
-		return  String.format(template, "raiz quadrada", Math.sqrt(validaNumero(numberOne)));
+	@GetMapping("/raiz/{number}")
+	public String raiz(@PathVariable String number) throws Exception {
+		return  String.format(template, "raiz quadrada", Math.sqrt(DoubleUtils.validaNumero(number)));
 	}
 
-	private Double validaNumero(String strNumber) throws Exception {
-		if (strNumber == null)
-			return 0D;
-		
-		if (!isNumber(strNumber)) {
-			throw new UnsupportedMathOperationException("Precisam ser numeros para a operação!");
-		} else {
-			strNumber.replaceAll(",", ".");
-		}
-		return converteDouble(strNumber);
-	}
-
-	private Double converteDouble(String strNumber) {		
-		return Double.parseDouble(strNumber);
-	}
-
-	private boolean isNumber(String strNumber) {
-		return strNumber.matches("[-+]?[0-9]*\\.?[0-9]+");
-	}
+	
 }
---
+++ src/main/java/br/bucker/bits/utils/DoubleUtils.java
@@ -0,0 +1,26 @@
+package br.bucker.bits.utils;
+
+import br.bucker.bits.exceptions.UnsupportedMathOperationException;
+
+public class DoubleUtils {
+	
+	public static Double validaNumero(String strNumber) throws Exception {
+		if (strNumber == null)
+			return 0D;
+		
+		strNumber = strNumber.replaceAll(",", ".");
+		if (!isNumber(strNumber)) {
+			throw new UnsupportedMathOperationException("Precisam ser numeros para a operação!");
+		}
+		
+		return converteDouble(strNumber);
+	}
+
+	public static Double converteDouble(String strNumber) {		
+		return Double.parseDouble(strNumber);
+	}
+
+	public static boolean isNumber(String strNumber) {
+		return strNumber.matches("[-+]?[0-9]*\\.?[0-9]+");
+	}
+}
